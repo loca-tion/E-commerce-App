9881f9395d9347a68998ffc70c920c481bd096c9
Sujal Sharma <95977443+loca-tion@users.noreply.github.com>
2024-03-30 16:12:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,"./build")));
+app.use(express.static('build'));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -30,9 +30,9 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 // rest api
-app.use('/*',function(req,res){
-  res.sendFile(path.join(__dirname,'./build/index.html'))
-})
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 //PORT
 const PORT = process.env.PORT || 8080;
